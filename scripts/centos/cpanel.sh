505f1f8b4a45d0782775338758866572fca61137
Josh Cox <josh@webhosting.coop>
2015-10-09 17:48:37 -0500

    added cloudlinux and systemctl start multi-user.target

--- scripts/centos/cpanel.sh
+++ scripts/centos/cpanel.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -eux
 
+systemctl start multi-user.target
+yum install -y deltarpm
+# systemctl enable multi-user.target
+sleep 5
 cd /home
 curl -o latest -L https://securedownloads.cpanel.net/latest
 sh latest
